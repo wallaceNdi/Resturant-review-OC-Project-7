21db31a8dc76585effb2ee3a412072f4b6400505
Tangwan wallace <42949636+wallaceNdi@users.noreply.github.com>
2019-11-26 21:32:36 +0100

    Add files via upload

---
+++ resturant-review project/js/places.js
@@ -0,0 +1,127 @@
+eqfeed_callback({
+
+    "html_attributions": [],
+    "results": [
+        {
+            "geometry": {
+                "location": {
+                    "lat": 39.58260557318532,
+                    "lng": 2.6307320594787598
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+            "id": 0,
+            "name": "wallace\'s Restaurant",
+            "formatted_phone_number": "6677666",
+            "url": "wallace.restaurant.com",
+            "website": "wallace.restaurant.com",
+            "opening_hours": {
+                "open_now": true
+            },
+            "rating": '4',
+            "reviews": [{
+                "name": "Devine",
+                "rating":5,
+                "text": "amazing food, best place ever",
+                "relative_time_description": "2 days ago"
+            }],
+
+            "place_id": "0",
+            "scope": "GOOGLE",
+            "types": ["restaurant"],
+            "vicinity": "Schloßstraße 101, 45355 Essen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.478099, 
+                    "lng": 6.983179
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+            "id": 1,
+            "name": "Wallace\'s Restaurant 2",
+            "formatted_phone_number": "76776862",
+            "url": "wallace.restaurant2.com",
+            "website": "wallace.restaurant2.com",
+            "opening_hours": {
+                "open_now": true
+            },
+            "rating": 4,
+            "reviews": [{
+                "name": "John",
+                "rating":4,
+                "text": "good food, good jazz music",
+                "relative_time_description": "3 days ago"
+            }],
+
+            "place_id": "1",
+            "scope": "GOOGLE",
+            "reference": "CoQBeQAAAFvf12y8veSQMdIMmAXQmus1zqkgKQ-O2KEX0Kr47rIRTy6HNsyosVl0CjvEBulIu_cujrSOgICdcxNioFDHtAxXBhqeR-8xXtm52Bp0lVwnO3LzLFY3jeo8WrsyIwNE1kQlGuWA4xklpOknHJuRXSQJVheRlYijOHSgsBQ35mOcEhC5IpbpqCMe82yR136087wZGhSziPEbooYkHLn9e5njOTuBprcfVw",
+            "types": ["restaurant"],
+            "vicinity": "Schloßstraße 101, 45355 Essen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.476816, 
+                    "lng": 6.986827,
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+            "id": 2,
+            "name": "wallace\'s Restaurant 3",
+            "formatted_phone_number": "6677666",
+            "url": "wallace.restaurant3.com",
+            "website": "wallace.restaurant3.com",
+            "opening_hours": {
+                "open_now": true
+            },
+            "rating": 2,
+            "reviews": [{
+                "name": "Linda",
+                "rating":2,
+                "text": "cocktail not so good, nice service ",
+                "relative_time_description": "3 days ago"
+            }],
+
+            "place_id": "2",
+            "scope": "GOOGLE",
+            "reference": "CoQBeQAAAFvf12y8veSQMdIMmAXQmus1zqkgKQ-O2KEX0Kr47rIRTy6HNsyosVl0CjvEBulIu_cujrSOgICdcxNioFDHtAxXBhqeR-8xXtm52Bp0lVwnO3LzLFY3jeo8WrsyIwNE1kQlGuWA4xklpOknHJuRXSQJVheRlYijOHSgsBQ35mOcEhC5IpbpqCMe82yR136087wZGhSziPEbooYkHLn9e5njOTuBprcfVw",
+            "types": ["restaurant"],
+            "vicinity": "Schloßstraße 101, 45355 Essen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.468629,  
+                    "lng":  6.942794
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+            "id": 3,
+            "name": "mass\'s Restaurant 4",
+            "formatted_phone_number": "76776864",
+            "url": "mass.restaurant4.com",
+            "website": "mass.restaurant4.com",
+            "opening_hours": {
+                "open_now": true
+            },
+            "rating": 4.6,
+            "reviews": [{
+                "name": "Sam",
+               "rating":4,
+               "text": "delicious meal",
+               "relative_time_description": "2 days ago"
+            }],
+
+            "place_id": "3",
+            "scope": "GOOGLE",
+            "reference": "CoQBeQAAAFvf12y8veSQMdIMmAXQmus1zqkgKQ-O2KEX0Kr47rIRTy6HNsyosVl0CjvEBulIu_cujrSOgICdcxNioFDHtAxXBhqeR-8xXtm52Bp0lVwnO3LzLFY3jeo8WrsyIwNE1kQlGuWA4xklpOknHJuRXSQJVheRlYijOHSgsBQ35mOcEhC5IpbpqCMe82yR136087wZGhSziPEbooYkHLn9e5njOTuBprcfVw",
+            "types": ["restaurant"],
+            "vicinity": "Dionysiuskirchplatz 2, 45355 Essen"
+        }
+    ],
+    "status": "OK"
+
+});
